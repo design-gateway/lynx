27f13ffb46f49b8476dbc847b499b2d768a4a5a6
dg-taksaporn <taksaporn@design-gateway.com>
2025-03-05 14:49:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@
 CONFIG_PATH="/home/ubuntu/tool/lynx/etc/DG.cfg"
 
 # Ensure the target directory exists
-mkdir -p /home/ubuntu/tool/lynx/etc
+sudo mkdir -p -m 777 /home/ubuntu/tool/lynx/etc
 
 # Create DG.cfg with the required network parameter
-echo "GATEWAY_IP:192.168.11.2" > "$CONFIG_PATH"
-echo "FPGA_IP:192.168.11.84" >> "$CONFIG_PATH"
-echo "FPGA_MAC:80.00.11.22.33.44" >> "$CONFIG_PATH"
+echo "GATEWAY_IP:192.168.11.2" | sudo tee "$CONFIG_PATH"
+echo "FPGA_IP:192.168.11.84" | sudo tee -a "$CONFIG_PATH"
+echo "FPGA_MAC:80.00.11.22.33.44" | sudo tee -a "$CONFIG_PATH"
 
 # Clean previous installations
 sudo make uninstall
